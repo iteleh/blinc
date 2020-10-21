f83c3730c90c1618ac4bc8eb0fc875fb10357219
Uduak Essien <acushla4real@gmail.com>
2020-10-21 16:20:31 +0100

    add nav and hero section

---
+++ src/modules/hero.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/prefer-default-export
+export const hero = `
+<div class="container">
+<div class="row align-items-center">
+    <div class="col-lg-6">
+        <div class="hero-title pr-lg-5">
+            <p class="text-uppercase text-primary font-weight-medium f-14 mb-4">One Stop LMS</p>
+            <h1 class="hero-1-title mb-4 font-weight-normal line-height-1_4">Learning has become easier. A one stop learning portal, get started today with <span class="text-primary font-weight-medium">BLINC</span></h1>
+            <p class="text-muted mb-4 pb-2">We have made BLINC so easy to setup that it won’t take you up to a day to get it deployed.</p>
+            <a href="" class="btn btn-warning">Find Out How <span class="ml-2 right-icon">&#8594;</span></a>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="mt-5 mt-lg-0">
+            <img src="assets/imgs/hero-1-img.png" alt="" class="img-fluid mx-auto d-block">
+        </div>
+    </div>
+</div>
+</div>
+`;
---
+++ src/modules/nav.js
@@ -0,0 +1,41 @@
+// eslint-disable-next-line import/prefer-default-export
+export const nav = `
+<div class="container">
+<!-- LOGO -->
+<a class="navbar-brand logo" href="index.html">
+  <img src="assets/imgs/htg-logo.png" alt="" class="" height="50" />
+</a>
+<button
+  class="navbar-toggler p-0"
+  type="button"
+  data-toggle="collapse"
+  data-target="#navbarCollapse"
+  aria-controls="navbarCollapse"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <i class="mdi mdi-menu"></i>
+</button>
+<div class="collapse navbar-collapse" id="navbarCollapse">
+  <ul class="navbar-nav ml-auto navbar-center" id="mySidenav">
+    <li class="nav-item active">
+      <a href="#home" class="nav-link">Home</a>
+    </li>
+    <li class="nav-item">
+      <a href="#services" class="nav-link">Services</a>
+    </li>
+    <li class="nav-item">
+      <a href="#about" class="nav-link">About</a>
+    </li>
+    <li class="nav-item">
+      <a href="#features" class="nav-link">Features</a>
+    </li>
+  </ul>
+  <div class="call-no">
+    <a href="#" class="nav-link text-warning"
+      ><i class="mdi mdi-phone mr-1"></i> +234 700 000 0000</a
+    >
+  </div>
+</div>
+</div>
+`;
