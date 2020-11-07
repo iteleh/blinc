92b1154631bf7f49388668d8292a6f4e00e34242
Uduak Essien <acushla4real@gmail.com>
2020-11-07 20:25:31 +0100

    update features content

--- src/modules/features.js
+++ src/modules/features.js
@@ -31,31 +31,25 @@ export const features = `
 
 
                         <div id="carouselContent" class="carousel" data-ride="carousel1">
-                        <ol class="carousel-indicators">
-                        <li data-target="#carouselContent" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselContent" data-slide-to="1"></li>
-                        <li data-target="#carouselContent" data-slide-to="2"></li>
-                      </ol>
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselContent" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselContent" data-slide-to="1"></li>
+                                <li data-target="#carouselContent" data-slide-to="2"></li>
+                            </ol>
                             <div class="carousel-inner" role="listbox">
-                                <div class="carousel-item active text-center p-4">
-                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Academic Result Management</h5>
-                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Manage students’ academic result right on the platform reducing paper work with the ability to set tests, examination customize your report cards the way you want. </p>
-                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Fees Management</h5>
-                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Manage school fees based on school sessions with the abilities to know who paid for or not.</p>
+                                <div class="carousel-item active p-4">
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Academic Result Management:</strong> Manage students’ academic result right on the platform reducing paper work with the ability to set tests, examination customize your report cards the way you want.</p>
+                                   
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Fees Management: </strong>Manage school fees based on school sessions with the abilities to know who paid for or not.</p>
                                 </div>
-                                <div class="carousel-item text-center p-4">
-                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Human Resource Management</h5>
-                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Record and organize your teachers, non-teaching staff and students details in one place enabling quick access to information and management of your school.</p>
-                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Collaboration Tools</h5>
-                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Makes it easy for the students and the teacher to have a class either through video conferencing, email and assessment of assignments, examination can be done on the platform. The student gets graded and see results at the end of the exams.</p>
+                                <div class="carousel-item p-4">
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Human Resource Management: </strong>Record and organize your teachers, non-teaching staff and students details in one place enabling quick access to information and management of your school.</p>
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Collaboration tools: </strong>Makes it easy for the students and the teacher to have a class either through video conferencing, email and assessment of assignments, examination can be done on the platform. The student gets graded and see results at the end of the exams.</p>
                                 </div>
-                                <div class="carousel-item text-center p-4">
-                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Library Access</h5>
-                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Books assigned per class for a term can be accessed in the library and downloaded into the student and teacher devices which can be accessed without the internet.</p>
-                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Report Card</h5>
-                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">The teacher has the ability to setup grading criteria for exams and assignment which makes up a customizable report card at the end of the term and the student and parent can access the results right on the platform.</p>
-                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">School Bus Shuttle Management</h5>
-                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Students who are day students who have paid for the school bus shuttle can be managed right on the platform without stress. </p>
+                                <div class="carousel-item p-4">
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Library access: </strong>Books assigned per class for a term can be accessed in the library and downloaded into the student and teacher devices which can be accessed without the internet.</p>
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Report card: </strong>The teacher has the ability to setup grading criteria for exams and assignment which makes up a customizable report card at the end of the term and the student and parent can access the results right on the platform.</p>
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>School bus shuttle management: </strong>Students who are day students who have paid for the school bus shuttle can be managed right on the platform without stress. </p>
                                 </div>
                                 
                             </div>
@@ -70,16 +64,39 @@ export const features = `
                 <div class="col-md-6">
                     <div class="features-box mb-4">
                         <div class="features-icon my-4">
-                            <i class="mdi mdi-account-group"></i>
+                        <i class="mdi mdi-account-group"></i> 
                         </div>
-                        <h5 class="text-dark font-weight-normal mb-3 pt-3">File Sharing</h5>
-                        <p class="text-muted mb-3 f-15">Share Lessons, Files, and collaborate alongside your students and co-teachers and students anytime, anywhere. Automatic Archive lesson is provided to move all expired lesson to a location that allow ease in navigation to new lesson.</p>
-                        <a href="#" class="f-16 text-warning">Read More <span class="right-icon ml-2">&#8594;</span></a>
+                        
+                        <h5 class="text-dark font-weight-normal pt-3">BLINC For Teachers</h5>
+                        <div id="carouselContentTeacher" class="carousel" data-ride="carousel1">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselContentTeacher" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselContentTeacher" data-slide-to="1"></li>
+                            <li data-target="#carouselContentTeacher" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active p-4">
+                                <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Homework management:</strong> On the BLINC platform, a teacher can set homework for the student, set a due date, track and assess them using our smart assessment tool right on the BLINC platform.  </p>
+                                <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Assess and grade:</strong> There is no need for the teacher to mark the students’ assignments or exams with pen, as the teacher can automate correct answers which grade the students automatically.</p>
+                            </div>
+                            <div class="carousel-item p-4">
+                                <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Attendance management:</strong> take note of people who attend classes and the number of students per class.</p>
+                                <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Report card:</strong>  The teacher has the ability to setup grading criteria for exams and assignment which makes up a customizable report card at the end of the term.</p>
+                            </div>
+                            <div class="carousel-item p-4">
+                                <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Library Access:</strong>   Teachers have access to books based on the curriculum per class and term which can be assigned to the students.</p>
+                                <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Collaboration tools:</strong>   makes it easy for the students and the teacher to have a class either through video conferencing, email and assessment of assignments, examination can be done on the platform. The student gets graded and see results at the end of the exams.</p>
+                            </div>
+                            
+                        </div>
+                    </div>
+
+
                     </div>
                 </div>
                 <div class="col-md-5 offset-md-1 mt-5 mt-sm-0" style="background: url(assets/imgs/features/features-bg.png) center center">
                     <div class="features-img">
-                        <img src="assets/imgs/features/img-2.png" alt="" class="img-fluid d-block mx-auto">
+                        <img src="assets/imgs/features/img-3.png" alt="" class="img-fluid d-block mx-auto">
                     </div>
                 </div>
             </div>
@@ -87,7 +104,7 @@ export const features = `
             <div class="row align-items-center mt-5">
                 <div class="col-md-5" style="background: url(assets/imgs/features/features-bg.png) center center">
                     <div class="features-img">
-                        <img src="assets/imgs/features/img-3.png" alt="" class="img-fluid d-block mx-auto">
+                        <img src="assets/imgs/features/img-2.png" alt="" class="img-fluid d-block mx-auto">
                     </div>
                 </div>
                 <div class="col-md-6 offset-md-1">
@@ -95,9 +112,38 @@ export const features = `
                         <div class="features-icon my-4">
                             <i class="mdi mdi-chart-bell-curve"></i>
                         </div>
-                        <h5 class="text-dark font-weight-normal mb-3 pt-3">Book Marketplace</h5>
-                        <p class="text-muted mb-3 f-15">Our Publishers find it easy to sell their eBooks and ineractive lessons to schools and to students, they are assured with a good ROI.</p>
-                        <a href="#" class="f-16 text-warning">Read More <span class="right-icon ml-2">&#8594;</span></a>
+                        <h5 class="text-dark font-weight-normal mb-3 pt-3">BLINC For Students</h5>
+
+                        <!-- 
+                         
+                        
+                        
+
+                        -->
+
+                        
+                        <div id="carouselContentStudents" class="carousel" data-ride="carousel1">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselContentStudents" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselContentStudents" data-slide-to="1"></li>
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active p-4">
+                                <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Library access:</strong> Books assigned per class for a term can be accessed in the library and downloaded into the student device which can be accessed without the internet.</p>
+                                <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Lesson Access:</strong>  Students can see the classes and subjects the teacher is teaching on the platform with options to do and submit assignments, take tests and examinations. </p>
+                            </div>
+                            <div class="carousel-item p-4">
+                                <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Collaboration tools:</strong>  makes it easy for the students and the teacher to have a class either through video conferencing, email and assessment of assignments, examination can be done on the platform. The student gets graded and see results at the end of the exams.</p>
+                                <p class="text-muted mb-3 f-15 pl-2 pr-2"><strong>Report Card: </strong>   The student gets to see his or her result on the platform at the end of the assignment, test or examination per subject as well as a customized report card at the end of the term with all the subjects.
+                                </p>
+                            </div>
+                        
+                            
+                        </div>
+                    </div>
+
+
+
                     </div>
                 </div>
 
