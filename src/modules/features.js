e3125be7878e632e06e8640d797afbcceb67b750
Uduak Essien <acushla4real@gmail.com>
2020-11-07 14:34:03 +0100

    add info slider for featured content

--- src/modules/features.js
+++ src/modules/features.js
@@ -21,9 +21,47 @@ export const features = `
                         <div class="features-icon my-4">
                             <i class="mdi mdi-laptop-mac"></i>
                         </div>
-                        <h5 class="text-dark font-weight-normal mb-3 pt-3">Course/Lesson Management</h5>
-                        <p class="text-muted mb-3 f-15">Our Lesson management feature allows Teachers and Trainers to easily create Lessons without guides. With its intuitive 7-Questions and advanced Custom Question-Answer Type Lessons. </p>
+                        <!--
+                        <h5 class="text-dark font-weight-normal mb-3 pt-3">Academic Result Management</h5>
+                        <p class="text-muted mb-3 f-15">Manage students’ academic result right on the platform reducing paper work with the ability to set tests, examination customize your report cards the way you want. </p>
+                        <h5 class="text-dark font-weight-normal mb-3 pt-3">Fees Management</h5>
+                        <p class="text-muted mb-3 f-15">Manage school fees based on school sessions with the abilities to know who paid for or not.</p>
                         <a href="#" class="f-16 text-warning">Read More <span class="right-icon ml-2">&#8594;</span></a>
+                        -->
+
+
+                        <div id="carouselContent" class="carousel" data-ride="carousel1">
+                        <ol class="carousel-indicators">
+                        <li data-target="#carouselContent" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselContent" data-slide-to="1"></li>
+                        <li data-target="#carouselContent" data-slide-to="2"></li>
+                      </ol>
+                            <div class="carousel-inner" role="listbox">
+                                <div class="carousel-item active text-center p-4">
+                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Academic Result Management</h5>
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Manage students’ academic result right on the platform reducing paper work with the ability to set tests, examination customize your report cards the way you want. </p>
+                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Fees Management</h5>
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Manage school fees based on school sessions with the abilities to know who paid for or not.</p>
+                                </div>
+                                <div class="carousel-item text-center p-4">
+                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Human Resource Management</h5>
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Record and organize your teachers, non-teaching staff and students details in one place enabling quick access to information and management of your school.</p>
+                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Collaboration Tools</h5>
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Makes it easy for the students and the teacher to have a class either through video conferencing, email and assessment of assignments, examination can be done on the platform. The student gets graded and see results at the end of the exams.</p>
+                                </div>
+                                <div class="carousel-item text-center p-4">
+                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Library Access</h5>
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Books assigned per class for a term can be accessed in the library and downloaded into the student and teacher devices which can be accessed without the internet.</p>
+                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">Report Card</h5>
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">The teacher has the ability to setup grading criteria for exams and assignment which makes up a customizable report card at the end of the term and the student and parent can access the results right on the platform.</p>
+                                    <h5 class="text-dark font-weight-normal mb-3 pt-3">School Bus Shuttle Management</h5>
+                                    <p class="text-muted mb-3 f-15 pl-2 pr-2">Students who are day students who have paid for the school bus shuttle can be managed right on the platform without stress. </p>
+                                </div>
+                                
+                            </div>
+                        </div>
+
+
                     </div>
                 </div>
             </div>
