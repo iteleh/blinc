9aa9728b9a875385477493c55cb5f64cc6e17d46
iteleh <iteleh97@gmail.com>
2020-11-18 12:42:56 +0100

    Update about.js

--- src/modules/about.js
+++ src/modules/about.js
@@ -16,7 +16,7 @@ export const about = `
                 <div class="col-md-7 offset-md-1">
                     <div class="row">
                         <div class="col-md-6">
-                            <h6 class="text-dark font-weight-light f-20 mb-3">Why BLINC</h6>
+                            <h6 class="text-dark font-weight-light f-20 mb-3">Why BLINCing</h6>
                             <ul class="text-muted font-weight-light">
                             <li>Manage your school business on the go, from your Internet enabled device</li>
                             <li>Customizable to suit your school's class naming system</li>
